6aa937ae1950b4e6f54145e81645b3856fd155b8
leonardobussi <leonardobezerrabussy@gmail.com>
2021-05-10 15:01:16 -0300

    inital

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# SimulaClientFrontend
+# Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.12.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.2.
 
 ## Development server
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { PostagemCriarComponent } from './views/postagens/postagem-criar/postagem-criar.component';
+import { PostagemCrudComponent } from './views/postagens/postagem-crud/postagem-crud.component';
+import { PostagemEditarComponent } from './views/postagens/postagem-editar/postagem-editar.component';
+import { PostagemVisualizarComponent } from './views/postagens/postagem-visualizar/postagem-visualizar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'postagem', component: PostagemCrudComponent},
+  {path: 'postagem/criar', component: PostagemCriarComponent},
+  {path: 'postagem/visualizar/:id', component: PostagemVisualizarComponent},
+  {path: 'postagem/editar/:id', component: PostagemEditarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
